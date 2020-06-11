aa68b9104a274910fab8366fe61ee7b2cf547ab4
Wojciech Słodziak <w.slodziak@polsource.com>
2020-06-11 13:33:57 +0200

    initial worker non working stage

---
+++ src/components/CrossbreedingSimulator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><textarea v-model="saplingGenes" /></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CrossbreedingSimulator extends Vue {
+  data() {
+    return {
+      saplingGenes: 'abc'
+    };
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import App from './App.vue';
+import * as workerPath from 'file-loader?name=[name].js!./worker';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app');
+
+const worker = new Worker(workerPath);
+
+console.log(workerPath, worker);
+worker.addEventListener('message', (message) => {
+  console.log(message);
+});
+worker.postMessage('this is a test message to the worker');

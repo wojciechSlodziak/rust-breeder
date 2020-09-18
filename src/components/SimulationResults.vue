848c32e07767fd92ec17f0abd230ce548df65253
Wojciech Słodziak <w.slodziak@polsource.com>
2020-09-18 12:04:01 +0200

    filtering by gene

--- src/components/SimulationResults.vue
+++ src/components/SimulationResults.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="text-center">
+    <v-container class="pa-0">
+      <v-row>
+        <v-col v-for="n in 6" :key="n">
+          <v-text-field type="text" :label="'Gene ' + n" v-model="filteringGenes['gene' + (n - 1)]"></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+
     <ul :class="{ 'simulation-results--visible': animate }">
       <li v-for="(map, index) in visibleMapList" :key="index">
         <SimulationMapRow :map="map" />
@@ -22,19 +30,40 @@ export default class SimulationResults extends Vue {
 
   animate = false;
 
-  // filteringGenes = {
-  //   gene0: null,
-  //   gene1: null,
-  //   gene2: null,
-  //   gene3: null,
-  //   gene4: null,
-  //   gene5: 'Y',
-  // }
+  filteringGenes: { [key: string]: string | null } = {
+    gene0: '',
+    gene1: '',
+    gene2: '',
+    gene3: '',
+    gene4: '',
+    gene5: ''
+  };
 
   page = 1;
 
   get filteredMapList() {
-    // const gene = Y
+    console.log(this.filteringGenes);
+    if (
+      this.filteringGenes.gene0 !== '' ||
+      this.filteringGenes.gene1 !== '' ||
+      this.filteringGenes.gene2 !== '' ||
+      this.filteringGenes.gene3 !== '' ||
+      this.filteringGenes.gene4 !== '' ||
+      this.filteringGenes.gene5 !== ''
+    ) {
+      return this.mapList.filter((map) => {
+        let allGenesMatch = true;
+        for (let i = 0; i < 6; i++) {
+          if (
+            this.filteringGenes[`gene${i}`] !== '' &&
+            map.targetSapling.genes[i].type !== (this.filteringGenes[`gene${i}`] || '').toUpperCase()
+          ) {
+            allGenesMatch = false;
+          }
+        }
+        return allGenesMatch;
+      });
+    }
     return this.mapList;
   }
 

c6651ca1c06e9f0de20fc372480f4ba777a4a2e9
Wociech Slodziak <wojciech.slodziak@gmail.com>
2022-04-23 23:00:01 +0200

    add temporary planning comments with

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,32 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  vuetify,
-  render: (h: Function) => h(App)
-}).$mount('#app');
+// new Vue({
+//   vuetify,
+//   render: (h: Function) => h(App)
+// }).$mount('#app');
+
+
+// TODO: rework this temp code into something good :)
+
+// Sapling we want to achieve, to be chosen from 3 possibilities.
+const targetSapling = 'GGYYYY';
+
+// User provided source saplings
+const sourceGenes = `GHXYGG
+YHYXGX
+HYXGGX
+HHGYXX
+GYHYXX`;
+
+// TODO: 
+// build them into Saplings
+// score based on resultSapling and on coverage of genes in sourceSaplings
+// sort by score
+// perform crossbreeding on limited set (start with 3 saplings taken from the sorted by score list)
+// if found something better then source saplings, add to another list firstStepResultSaplings
+// after crossbreeding for limited set is done, perform crossbreeding with all saplings from firstStepResultSaplings and that set
+// if found perfect show it in the ui
+// increase the limited set size by 1 (add next best sapling from the list, make sure to not crossbreed the same combinations again)
+
+

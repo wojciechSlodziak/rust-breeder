7e9a4bdf63b341e278645c4de65e1bd9ab4743e4
Wojciech Słodziak <wojciech.slodziak@gmail.com>
2022-12-08 15:52:46 +0100

    fix wrong second and minute display

--- src/lib/time-utils.ts
+++ src/lib/time-utils.ts
@@ -3,6 +3,7 @@ export function timeMsToTimeString(timeMs: number): string {
   const minutes = Math.floor((timeMs % 3600000) / 60000);
   const seconds = (timeMs % 60000) / 1000;
   const hoursPart = hours > 0 ? `${hours}h:` : '';
-  const minutesPart = hours > 0 || minutes > 0 ? `${minutes < 10 ? '0' : ''}${minutes}m:` : '';
-  return `${hoursPart}${minutesPart}${seconds.toFixed(0)}s`;
+  const minutesPart = hours > 0 || minutes > 0 ? `${hours > 0 && minutes < 10 ? '0' : ''}${minutes}m:` : '';
+  const secondsPart = `${(hours > 0 || minutes > 0) && seconds < 10 ? '0' : ''}${seconds.toFixed(0)}s`;
+  return `${hoursPart}${minutesPart}${secondsPart}`;
 }

046166b4dcb295a2120a941db1e8d009c19d2acd
Wojciech Słodziak <wojciech.slodziak@gmail.com>
2024-01-07 21:47:28 +0100

    fix time display  = 1m:60s

--- src/lib/time-utils.ts
+++ src/lib/time-utils.ts
@@ -4,6 +4,6 @@ export function timeMsToTimeString(timeMs: number): string {
   const seconds = (timeMs % 60000) / 1000;
   const hoursPart = hours > 0 ? `${hours}h:` : '';
   const minutesPart = hours > 0 || minutes > 0 ? `${hours > 0 && minutes < 10 ? '0' : ''}${minutes}m:` : '';
-  const secondsPart = `${(hours > 0 || minutes > 0) && seconds < 10 ? '0' : ''}${seconds.toFixed(0)}s`;
+  const secondsPart = `${(hours > 0 || minutes > 0) && seconds < 10 ? '0' : ''}${Math.floor(seconds)}s`;
   return `${hoursPart}${minutesPart}${secondsPart}`;
 }
